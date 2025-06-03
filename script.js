ffffefb1a6b0bb92dceadef1012d2455ab10c718
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-03 16:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	let index=' '
+	for(let i=0;i<text.length;i++){
+		if(text[i]!=' ') {
+			index=text[i]
+			break
+		}
+	}
+	return index
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
